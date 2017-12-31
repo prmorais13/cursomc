5ad47bb6d6ac1aef0c17923981f717ca0fb8a231
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2017-12-31 00:33:13 -0200

    Produto e associação muito para muitos com Categoria

--- src/main/java/com/pauloroberto/cursomc/domains/Produto.java
+++ src/main/java/com/pauloroberto/cursomc/domains/Produto.java
@@ -8,28 +8,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data //Anotação do lombox que gera getters, setters, hascode e equals automático
-@NoArgsConstructor //Anotação do lombox que gera construtor vazio automático
-public class Categoria implements Serializable {
+@Data
+@NoArgsConstructor
+public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Categoria(Integer id, String nome) {
+	@ManyToMany
+	@JoinTable(name = "produto_categoria",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
+
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 }
