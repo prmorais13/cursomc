6350182056a09efc706333651f5a7d3625acf994
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2017-12-30 23:17:36 -0200

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/pauloroberto/cursomc/domains/Categoria.java
+++ src/main/java/com/pauloroberto/cursomc/domains/Categoria.java
@@ -2,12 +2,20 @@ package com.pauloroberto.cursomc.domains;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
-@Data
+@Entity
+@Data //Anotação do lombox que gera getters, setters, hascode e equals automático
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
