3cef797eaee934b4d67f6d8f18e25044ca6d7a0b
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-01-04 21:11:14 -0200

    Criando profile de test

--- src/main/java/com/pauloroberto/cursomc/CursomcApplication.java
+++ src/main/java/com/pauloroberto/cursomc/CursomcApplication.java
@@ -1,65 +1,11 @@
 package com.pauloroberto.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.pauloroberto.cursomc.domains.Categoria;
-import com.pauloroberto.cursomc.domains.Cidade;
-import com.pauloroberto.cursomc.domains.Cliente;
-import com.pauloroberto.cursomc.domains.Endereco;
-import com.pauloroberto.cursomc.domains.Estado;
-import com.pauloroberto.cursomc.domains.ItemPedido;
-import com.pauloroberto.cursomc.domains.Pagamento;
-import com.pauloroberto.cursomc.domains.PagamentoComBoleto;
-import com.pauloroberto.cursomc.domains.PagamentoComCartao;
-import com.pauloroberto.cursomc.domains.Pedido;
-import com.pauloroberto.cursomc.domains.Produto;
-import com.pauloroberto.cursomc.domains.enums.EstadoPagamento;
-import com.pauloroberto.cursomc.domains.enums.TipoCliente;
-import com.pauloroberto.cursomc.repositories.CategoriaRepository;
-import com.pauloroberto.cursomc.repositories.CidadeRepository;
-import com.pauloroberto.cursomc.repositories.ClienteRepository;
-import com.pauloroberto.cursomc.repositories.EnderecoRepository;
-import com.pauloroberto.cursomc.repositories.EstadoRepository;
-import com.pauloroberto.cursomc.repositories.ItemPedidoRepository;
-import com.pauloroberto.cursomc.repositories.PagamentoRepository;
-import com.pauloroberto.cursomc.repositories.PedidoRepository;
-import com.pauloroberto.cursomc.repositories.ProdutoRepository;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	
-	@Autowired
-	private EstadoRepository estadoRepository;
-	
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	
-	@Autowired
-	private PagamentoRepository pagamentoRepositoy;
-	
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -68,106 +14,5 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
-		Categoria cat5 = new Categoria(null, "Jardinagem");
-		Categoria cat6 = new Categoria(null, "Decoração");
-		Categoria cat7 = new Categoria(null, "Perfumaria");
-		Categoria cat8 = new Categoria(null, "Esporte");
-		
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
-		Produto p5 = new Produto(null, "Tolha", 50.00);
-		Produto p6 = new Produto(null, "Colcha", 200.00);
-		Produto p7 = new Produto(null, "TV 65 Polegadas Smart", 2500.00);
-		Produto p8 = new Produto(null, "Roçadeira", 800.00);
-		Produto p9 = new Produto(null, "Abajour", 100.00);
-		Produto p10 = new Produto(null, "Pendente", 180.00);
-		Produto p11 = new Produto(null, "Shampoo", 30.00);
-		Produto p12 = new Produto(null, "Bola de Futebol", 150.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
-		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
-		cat5.getProdutos().addAll(Arrays.asList(p8));
-		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
-		cat7.getProdutos().addAll(Arrays.asList(p11));
-		cat8.getProdutos().addAll(Arrays.asList(p12));
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p4.getCategorias().addAll(Arrays.asList(cat2));
-		p5.getCategorias().addAll(Arrays.asList(cat3));
-		p6.getCategorias().addAll(Arrays.asList(cat3));
-		p7.getCategorias().addAll(Arrays.asList(cat4));
-		p8.getCategorias().addAll(Arrays.asList(cat5));
-		p9.getCategorias().addAll(Arrays.asList(cat6));
-		p10.getCategorias().addAll(Arrays.asList(cat6));
-		p11.getCategorias().addAll(Arrays.asList(cat7));
-		p12.getCategorias().addAll(Arrays.asList(cat8));
-		
-		
-		this.categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
-		this.produtoRepository.save(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12));
-		
-		Estado est1 = new Estado(null, "Paraíba");
-		Estado est2 = new Estado(null, "Rio Grande do Norte");
-		
-		Cidade c1 = new Cidade(null, "João Pessoa", est1);
-		Cidade c2 = new Cidade(null, "Natal", est2);
-		Cidade c3 = new Cidade(null, "Parnamirim", est2);
-		
-		est1.getCidades().add(c1);
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
-		this.estadoRepository.save(Arrays.asList(est1, est2));
-		this.cidadeRepository.save(Arrays.asList(c1, c2, c3));
-		
-		
-		Cliente cli1 = new Cliente(null, "Paulo Roberto", "prmorais_13@hotmail.com", "39135810491", TipoCliente.PESSOAFISICA);
-		
-		Endereco e1 = new Endereco(null, "Rua Parque Paraúna", "79", "Parque Verde", "Nova Esperança", "59144170", cli1, c3);
-		Endereco e2 = new Endereco(null, "Rua Princesa Isabel", "799", null, "Cidade Alta", "59025400", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		cli1.getTelefones().addAll(Arrays.asList("32328877", "987015547"));
-		
-		this.clienteRepository.save(cli1);
-		this.enderecoRepository.save(Arrays.asList(e1, e2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-		
-		Pedido ped1 = new Pedido(null, sdf.parse("13/02/2018 13:00"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("04/05/2018 22:50"), cli1, e2);
-		
-		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped2, 1);
-		ped2.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped1, sdf.parse("04/06/2018 22:50"), null);
-		ped1.setPagamento(pagto2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
-		this.pedidoRepository.save(Arrays.asList(ped1, ped2));
-		this.pagamentoRepositoy.save(Arrays.asList(pagto1, pagto2));
-		
-		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
-		
-		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
-		ped2.getItens().addAll(Arrays.asList(ip3));
-		
-		p1.getItens().addAll(Arrays.asList(ip1));
-		p2.getItens().addAll(Arrays.asList(ip3));
-		p3.getItens().addAll(Arrays.asList(ip2));
-		
-		this.itemPedidoRepository.save(Arrays.asList(ip1, ip2, ip3));
 	}
 }
